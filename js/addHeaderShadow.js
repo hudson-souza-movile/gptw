7b357b9da8e21243d26ff567350c0d2239511c38
hudsonmacedo <hudsonmacedo14@gmail.com>
2017-03-20 11:29:41 -0300

    first part of pilar page done

--- js/addHeaderShadow.js
+++
@@ -1,7 +0,0 @@
-u(document).on('scroll', function(){                        
-	if (window.scrollY > 10) {
-		u('header').addClass('shadow-header');
-	} else {
-		u('header').removeClass('shadow-header');
-	}
-});
---
+++ js/changeHeader.js
@@ -0,0 +1,11 @@
+u(document).on('scroll', function(){                        
+	if (window.scrollY > 10) {
+		u('header.home').addClass('shadow-header');
+		u('header.pilar').addClass('scrolled');
+		u('div.trigger').removeClass('trigger-pilar');
+	} else {
+		u('header.home').removeClass('shadow-header');
+		u('header.pilar').removeClass('scrolled');
+		u('div.trigger').addClass('trigger-pilar');
+	}
+});
